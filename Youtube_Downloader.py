f51fcc952c16f8c5704cc6182cbdd80f07b853a4
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-09-17 00:23:48 -0100

    bug fixed

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -48,6 +48,7 @@ def single_video(url):
 def video_playlist(url):
         
         playlist = Playlist(url)
+        
         try:
                 try:    
                         title = playlist.title
@@ -60,14 +61,13 @@ def video_playlist(url):
                 print("---------------------------------DOWNLOADING...-----------------------------\n")
                 count = 1
                 for video in playlist.videos:
-                        if count>=61:
-                                try:
-                                        video.register_on_progress_callback(on_progress)
-                                        print(f"Downloading({count}/{len(playlist.videos)}):{video.title}")
-                                        video.streams.get_highest_resolution().download()
-                                        print("\n")
-                                except Exception as exception:
-                                        print(f"Failed downloading:{video.title} | Error:{exception}")
+                        try:
+                                video.register_on_progress_callback(on_progress)
+                                print(f"Downloading({count}/{len(playlist.videos)}):{video.title}")
+                                video.streams.get_highest_resolution().download()
+                                print("\n")
+                        except Exception as exception:
+                                print(f"Failed downloading:{count}/{len(playlist.videos)}:{video.title} | Error:{exception}")
                         count=count+1
                 print("------------------------PLAYLIST DOWNLOAD COMPLETE--------------------------\n")
                 os.chdir(dir_path)
@@ -103,25 +103,26 @@ def audio_playlist(url):
         except Exception as exception:
                 print(f"Connection Error: {exception}")
                 os.chdir(dir_path)
-
-""" while True:
-                        
-        chc = input("\n================================\n             MENU            \n================================\n1 - Single Video\n2 - Video Playlist\n3 - Single Audio\n4 - Audio Playlist\n5 - Exit\n================================\nEnter a choice and press enter:")
-
-        if chc == "1":
-                url = input("Please Enter the URL:")
-                single_video(url)
-        elif chc == "2":
-                url = input("Please Enter the URL:")
-                video_playlist(url)
-        elif chc == "3":
-                url = input("Please Enter the URL:")
-                single_audio(url)
-        elif chc == "4":
-                url = input("Please Enter the URL:")
-                audio_playlist(url)
-        elif chc == "5":
-                print("Exiting...")
-                break """
+                
+if __name__ == "__main__":
+	while True:
+	                        
+	        chc = input("\n================================\n             MENU            \n================================\n1 - Single Video\n2 - Video Playlist\n3 - Single Audio\n4 - Audio Playlist\n5 - Exit\n================================\nEnter a choice and press enter:")
+	
+	        if chc == "1":
+	                url = input("Please Enter the URL:")
+	                single_video(url)
+	        elif chc == "2":
+	                url = input("Please Enter the URL:")
+	                video_playlist(url)
+	        elif chc == "3":
+	                url = input("Please Enter the URL:")
+	                single_audio(url)
+	        elif chc == "4":
+	                url = input("Please Enter the URL:")
+	                audio_playlist(url)
+	        elif chc == "5":
+	                print("Exiting...")
+	                break 
 
         
